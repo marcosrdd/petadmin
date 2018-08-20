5e6629e0814c2d2402232816b628659d2a45d3a7
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-08-19 23:14:42 -0300

    calculating total sell

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,17 +1,18 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
-  enum status: { finished: 0, canceled: 1 }
-
+  enum status: { finalizada: 0, cancelada: 1 }
+  validates :client, presence: true
+ 
   belongs_to :discount
   belongs_to :client
-
+ 
   has_many :sell_products
   has_many :products, through: :sell_products
-  
+ 
   has_many :sell_services
   has_many :services, through: :sell_services
-
-  validates :client, presence: true
+ 
+  before_save :set_total
  
   def fae_display_field
     id
@@ -21,4 +22,18 @@ class Sell < ApplicationRecord
     order(:id)
   end
  
+  private
+ 
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
+ 
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+ 
+    total = (total &gt;= 0)? total : 0
+    self.total = total
+  end
 end
